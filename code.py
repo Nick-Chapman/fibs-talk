c7d78d0ecf088c44ca93ff905cf2946dce92ed81
Nick Chapman <nic@amadido.co.uk>
2021-03-26 14:08:36 +0000

    update code & talk after a self runthough

--- code.py
+++ code.py
@@ -2,36 +2,37 @@
 # code fragments used in the talk
 
 def fib(n):
-    if n < 2: return n
+    if n == 0: return 0
+    if n == 1: return 1
     else: return fib(n-1) + fib(n-2)
 
-def sequence():
-    for i in range(1,35):
+def seq():
+    for i in range(1,38):
         answer = fib(i)
         print (i, answer)
 
 import time
 
-def timed_sequence():
-    for i in range(1,35):
+def seqTimed():
+    for i in range(1,38):
         start = time.time()
         answer = fib(i)
         end = time.time()
         print (i, answer, end - start)
 
-import math
-phi = (math.sqrt(5)+1)/2
-
 def qfib(n):
-    if n < 2: return n
-    else:
-        a, b, i  = 0, 1, 1
-        while (i < n):
-            c = a + b
-            a, b, i = b, c, i+1
-        return b
+    if n == 0: return 0
+    if n == 1: return 1
+    a,b = 0,1
+    step = 1
+    while (step < n):
+        new = a+b
+        a = b
+        b = new
+        step = step + 1
+    return b
 
-def sequence200():
+def seq200():
     for i in range(1,201):
         answer = qfib(i)
         print (i, answer)
