1ebf1d84d3de7b5c870cfb06a6accf5ad0b0e55a
Nick Chapman <nic@amadido.co.uk>
2021-03-27 17:58:39 +0000

    mods following 2nd runthrough with Ellie

--- code.py
+++ code.py
@@ -1,10 +1,11 @@
 
 # code fragments used in the talk
 
+# original slow (exponential) version
 def fib(n):
     if n == 0: return 0
     if n == 1: return 1
-    else: return fib(n-1) + fib(n-2)
+    return fib(n-1) + fib(n-2)
 
 def seq():
     for i in range(1,38):
@@ -20,6 +21,7 @@ def seqTimed():
         end = time.time()
         print (i, answer, end - start)
 
+# quick (linear) version; coded using a loop
 def qfib(n):
     if n == 0: return 0
     if n == 1: return 1
@@ -27,12 +29,17 @@ def qfib(n):
     step = 1
     while (step < n):
         new = a+b
-        a = b
-        b = new
+        a,b = b,new
         step = step + 1
     return b
 
-def seq200():
-    for i in range(1,201):
-        answer = qfib(i)
-        print (i, answer)
+# using fib to compute phi
+phi = qfib(200) / qfib(199)
+
+# quick (linear) version; coded using tail recursion
+def tfibW (a,b,step):
+    if step == 0: return b
+    return tfibW (b, a+b, step-1)
+
+def tfib(n):
+    return tfibW(0,1,n-1)
